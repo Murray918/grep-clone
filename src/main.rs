9e4d36dfbbc05186ef95f81eadc8f4ecf0a68574
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 13:09:41 -0500

    added a for loop that will search throught the args provided and print out the lines that are contained in the arguments

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,10 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
     let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    println!("{}", content)
+    // using a for in loop we loop through the lines of content and if the line contains one of the arguments we print it to stdout
+    for line in content.lines() {
+    if line.contains(&args.pattern) {
+        println!("{}", line);
+    }
+}
 }   
