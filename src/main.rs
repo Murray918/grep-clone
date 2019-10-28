f2ff94823e1237a05ec7908928695ba0c25e492f
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 14:20:50 -0500

    playing with some things right now trying to get all the struct variables to work

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
-
-
-use failure::ResultExt;
 use exitfailure::ExitFailure;
+use failure::ResultExt;
+use std::path::Path;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
 fn main() -> Result<(), ExitFailure> {
-    let path = "party.txt";
-    let content = std::fs::read_to_string(path)
+    // let path = "party.txt";
+    let args = Cli::from_args();
+    let path = Path::display(&args.path);
+    let content = std::fs::read_to_string(&args.path)
         .with_context(|_| format!("could not read file `{}`", path))?;
-    println!("file content: {}", content);
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("file content: {}", content);
+        }
+    }
     Ok(())
 }
-
