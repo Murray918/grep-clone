b5de7ae6296357f12f3a3bf9f5a84b70649ffb66
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 13:11:49 -0500

    reformat and commented out line 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+// use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -14,10 +14,11 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
     let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
     // using a for in loop we loop through the lines of content and if the line contains one of the arguments we print it to stdout
     for line in content.lines() {
     if line.contains(&args.pattern) {
         println!("{}", line);
+        }
     }
-}
 }   
