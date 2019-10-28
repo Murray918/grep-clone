5e948fac80c439231924a1c3dcdd9a5844639db1
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 13:38:18 -0500

    removed dead code and added some linting options at the top

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-// use std::path::PathBuf;
-use structopt::StructOpt;
-
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(StructOpt)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
-
+#![allow(unused_variables)]
+#![allow(unused_imports)]
 fn main() {
-    let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
-    // using a for in loop we loop through the lines of content and if the line contains one of the arguments we print it to stdout
-    for line in content.lines() {
-    if line.contains(&args.pattern) {
-        println!("{}", line);
-        }
-    }
-}   
+let result = std::fs::read_to_string("test.txt");
+let content = match result {
+    Ok(content) => { content },
+    Err(error) => { panic!("Can't deal with {}, just exit here", error); }
+};
+println!("file content: {}", content);
+}
