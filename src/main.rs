afaeacd6f22f91b9023ad6e22345a5df2292dd64
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 13:06:25 -0500

    the program now prints the contents of a file to the terminal it is compiled with a slew of warnings though

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
 }
 
-let pattern = std::env::args().nth(1).expect("no pattern given")
-let path = std::env::args().nth(2).expect("no path given")
+fn main() {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    println!("{}", content)
+}   
