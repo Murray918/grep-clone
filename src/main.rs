cdf1b78e4f649a2eff059e138561dbe37e59ad7a
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 15:15:38 -0500

    reorganized and created a test for the find_matches function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-
-use exitfailure::ExitFailure;
-use failure::ResultExt;
-use std::path::Path;
-use std::path::PathBuf;
 use structopt::StructOpt;
+use failure::ResultExt;
+use exitfailure::ExitFailure;
 
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
     /// The pattern to look for
@@ -15,16 +11,28 @@ struct Cli {
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
+
 fn main() -> Result<(), ExitFailure> {
-    // let path = "party.txt";
     let args = Cli::from_args();
-    let path = Path::display(&args.path);
     let content = std::fs::read_to_string(&args.path)
-        .with_context(|_| format!("could not read file `{}`", path))?;
+        .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
+
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("file content: {}", content);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
         }
     }
-    Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
